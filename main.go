11a97e53359f3d8a88a2656b742b9edf96dc9f18
Felipe <feliperdsiqueira@gmail.com>
2024-01-15 22:39:29 -0300

    feat: refactory

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 
@@ -14,16 +12,8 @@ type BodyResponse struct {
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	rawBody := request.Body
-	fmt.Println("Start")
-	fmt.Println(request.Body)
-	fmt.Println(request.Headers)
-	fmt.Println(request)
-	fmt.Println("Body: ", rawBody)
+	isVerified := signature.Handle(request.Headers, request.Body)
 
-	isVerified := signature.Handle(request.Headers, rawBody)
-
-	// Respond accordingly
 	if !isVerified {
 		return events.APIGatewayProxyResponse{
 			Body:       "invalid request signature",
